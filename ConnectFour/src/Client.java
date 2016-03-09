0942df3a358ad59c10682f0d9338a2bc291e0a8c
subhodeepr <subhodeepr@gmail.com>
2016-03-08 23:46:22 -0700

    initial commit for client/server/authentication classes

--- ConnectFour/src/Client.java
+++ ConnectFour/src/Client.java
@@ -1,4 +1,44 @@
+import java.io.*;
+import java.net.*;
 
 public class Client {
+
+    public static void main(String args[]) throws Exception {
+	if (args.length != 1)
+        {
+            System.out.println("Usage: Client <Server IP>");
+            System.exit(1);
+        }
+	String address = args[0];
+	Socket socket = null;
+	String line = null;
+	BufferedReader bufferedReader = null;
+	BufferedReader inputStream = null;
+	PrintWriter outputStream = null;
+
+	try {
+	    socket = new Socket(address, 9999); 
+	    bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	    inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    outputStream = new PrintWriter(socket.getOutputStream());
+	    
+	    Authentication authentication = new Authentication(inputStream, outputStream, bufferedReader, socket);
+	    authentication.authenticate();
+
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.err.print("IO Exception");
+	}
+
+	finally {
+	    inputStream.close();
+	    outputStream.close();
+	    bufferedReader.close();
+	    socket.close();
+	    System.out.println("Logged out from server");
+
+	}
+
+    }
 
 }
--- ConnectFour/src/Server.java
+++ ConnectFour/src/Server.java
@@ -1,4 +1,99 @@
+import java.io.*;
+import java.net.*;
 
 public class Server {
 
+    public static void main(String args[]) {
+
+	Socket s = null;
+	ServerSocket ss2 = null;
+	System.out.println("ConnectFour server started");
+	try {
+	    ss2 = new ServerSocket(9999);
+
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.out.println("Server error");
+
+	}
+
+	while (true) {
+	    try {
+		s = ss2.accept();
+		System.out.println("Connection established from " + s.getLocalAddress());
+		ServerThread st = new ServerThread(s);
+		st.start();
+
+	    }
+
+	    catch (Exception e) {
+		e.printStackTrace();
+		System.out.println("Connection Error");
+
+	    }
+	}
+
+    }
+}
+
+class ServerThread extends Thread {
+
+    String line = null;
+    BufferedReader inputStream = null;
+    PrintWriter outputStream = null;
+    Socket s = null;
+
+    public ServerThread(Socket s) {
+	this.s = s;
+    }
+
+    public void run() {
+	try {
+	    inputStream = new BufferedReader(new InputStreamReader(s.getInputStream()));
+	    outputStream = new PrintWriter(s.getOutputStream());
+
+	} catch (IOException e) {
+	    System.out.println("IO error in server thread");
+	}
+
+	try {
+	    
+	    line = inputStream.readLine();
+	    while (!line.equalsIgnoreCase("logout")) {
+		outputStream.println(line);
+		outputStream.flush();
+		System.out.println("Response to Client  :  " + line);
+		line = inputStream.readLine();
+	    }
+	} catch (IOException e) {
+
+	    line = this.getName(); // reused String line for getting thread name
+	    System.out.println("IO Error/ Client " + line + " terminated abruptly");
+	} catch (NullPointerException e) {
+	    line = this.getName(); // reused String line for getting thread name
+	    System.out.println("Client " + line + " Closed");
+	}
+
+	finally {
+	    try {
+		System.out.println("Connection Closing..");
+		if (inputStream != null) {
+		    inputStream.close();
+		    System.out.println(" Socket Input Stream Closed");
+		}
+
+		if (outputStream != null) {
+		    outputStream.close();
+		    System.out.println("Socket Out Closed");
+		}
+		if (s != null) {
+		    s.close();
+		    System.out.println("Socket Closed");
+		}
+
+	    } catch (IOException ie) {
+		System.out.println("Socket Close Error");
+	    }
+	} // end finally
+    }
 }
