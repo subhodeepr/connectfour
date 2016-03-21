e746c8b73779f9de564e916f929c4f76803df5d3
equach09 <equach95@gmail.com>
2016-03-21 00:24:34 -0600

    Added my part
    I added the diagonal check for a winner and also did the check if the
    column is full

--- ConnectFour/src/GameLogic.java
+++ ConnectFour/src/GameLogic.java
@@ -5,12 +5,17 @@ public class GameLogic {
 	private Boolean pause_game;
 
 	// return if move is valid or not
-	public Boolean validateMove(String move){
+	public Boolean validateMove(String move, String[][] board){
 		int user_input = (int) move.charAt(0);
 		char base_value = 'A';
 		int ascii_value = (int) base_value;
 		
 		if ((user_input - ascii_value) <= 6){
+			if (board[0][user_input - ascii_value] == "X" || board[0][user_input - ascii_value] == "O"){
+					System.out.println("The column is full. Please enter another move.");
+					numberOfTurns++;
+					return false;
+			}
 			numberOfTurns++;
 			return true;
 		}
@@ -63,7 +68,42 @@ public class GameLogic {
 	}
 	
 	public void checkDiagonal(String[][] board){
-		
+		// top left to bottom right check
+		int temp = 0;
+		for(int i = 0; i < 7; i++){
+			temp = i;
+			int count_tokens = 0;
+			for (int j = 0; j < 6; j++){
+				if (board[j][temp].equals("X")){	// player_token goes here to check for who wins
+					count_tokens = count_tokens + 1;
+					if (temp < 6)
+						temp++;
+					if (count_tokens == 4){
+						win_state = true;
+					}
+				}
+				else{
+					count_tokens = 0;
+				}
+			}
+		}
+		// bottom left to top right check
+		for (int i = 5; i > 0; i--){
+			temp = i;
+			int count_tokens = 0;
+			for (int j = 0; j < 7; j++){
+				if (board[temp][j].equals("X")){
+					count_tokens = count_tokens + 1;
+					if (temp >= 1)
+						temp--;
+					if (count_tokens == 4){
+						win_state = true;
+					}
+				}
+				else
+					count_tokens = 0;
+			}
+		}
 	}
 	
 }
